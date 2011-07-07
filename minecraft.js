09d3c45946e1bcff1fc1280897c1fe249253df29
root <root@mail.oom.ch>
2011-07-07 19:45:11 +0200

    fixes

--- minecraft.js
+++ minecraft.js
@@ -1,4 +1,4 @@
-#!/opt/node/node
+#!/usr/bin/env node
 
 var sys = require('sys'),
     fs = require('fs'),
@@ -7,7 +7,7 @@ var sys = require('sys'),
     spawn = require('child_process').spawn,
     stdin = process.openStdin(),
     util = require('util'),
-    minecraft = spawn('/usr/bin/java',['\-Xms1g','\-Xmx1g','\-jar','\minecraft_server.jar','\nogui','/home/minecraft/minecraft-testserver']);
+    minecraft = spawn('/usr/bin/java',['\-Xms1g','\-Xmx1g','\-jar','\/home/minecraft/minecraft-testserver/minecraft_server.jar','nogui']);
 
 
 /*process.ARGV[2];
@@ -21,11 +21,14 @@ console.log("Node Memory Usage "+util.inspect(process.memoryUsage()));
 sys.puts("\n\nMinecraft launched on PID: " + minecraft.pid);
 sys.puts("Webconsole available on  http://127.0.0.1:1337/ ");
 
+
 minecraft.stdout.on('data', function (data) {
     console.log('stdout: ' + data);
 });
 
+/* get triggeret on jvm stdout activity */
 minecraft.stderr.on('data', function (data) {
+    /*TODO pharse logfile stream, count users, trigger on meta command*/
     console.log('stderr: ' + data);
 });
 
@@ -34,36 +37,29 @@ minecraft.on('exit', function (code) {
     res.write(data);
 });
 
+/* http webservice */
 http.createServer(function (req, res) {
-
     res.writeHead(200, {'Content-Type': "text/plain;charset=UTF-8"});
-    res.write("this is a chunk streamn");
-    data="say New Nodemine HTTP connection"
-    minecraft.stdin.write(data + '\n');
-    res.write(data);
-
-  
+    minecraft.stdin.write('say Welcome to Nodemine stranger HTTP client\n');
+    res.write("");
     minecraft.stdout.on('data', function (data) {
-
         res.write(data);
     });
-
     minecraft.stderr.on('data', function (data) {
-        /*pharse logfile stream, count users, trigger on meta command*/
+        /*TODO pharse logfile stream, count users, trigger on meta command*/
         res.write(data);
     });
-
 }).listen(1337);
 
 
-/* catching all console input */
+/* catching all console input and wraps it to stdin of the java subprocess*/
 stdin.on('keypress', function (chunk, key) {
     process.stdout.write(chunk);
     minecraft.stdin.write(chunk);
     process.stdin.resume();
     if (key && key.ctrl && key.name == 'c') {
         sys.puts("CTRL+C detected!!! KILL KILL KILL! X_x");
-        minecraft.stdin.write("stop");
+        minecraft.stdin.write("stop\n");
         minecraft.stdin.end();
         minecraft.kill('SIGHUP');
         process.exit();
