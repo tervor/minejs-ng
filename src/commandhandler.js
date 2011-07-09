13ce04c21c552bc3aeb946ad410fa193df08e68f
Simon Kallweit <simon.kallweit@intefo.ch>
2011-07-09 23:43:44 +0200

    added items and give, items commands

--- src/commandhandler.js
+++ src/commandhandler.js
@@ -1,6 +1,7 @@
 
 var events = require('events');
 var util = require('util');
+var fs = require('fs');
 
 // The CommandHandler class implements all the advanced commands that can be
 // issued through the different interfaces (http, ingame, telnet) to the
@@ -8,10 +9,19 @@ var util = require('util');
 function CommandHandler(mcserver) {
 	events.EventEmitter.call(this);
 	this.mcserver = mcserver;
+	
+	for (var i = 0; i < this.items.length; i++) {
+		this.items[i].name = this.items[i].name.replace(' ', '_').toLowerCase();
+	}
+	
+	console.log(util.inspect(this.items));
+	
 }
 
 util.inherits(CommandHandler, events.EventEmitter);
 
+// Load item list
+CommandHandler.prototype.items = JSON.parse(fs.readFileSync('./src/items.json', 'ascii'));
 
 // List of handlers to be called when commands are executed.
 // Make sure that a handler function of type cmd_xxx(user, args) with the same name actually
@@ -19,6 +29,8 @@ util.inherits(CommandHandler, events.EventEmitter);
 CommandHandler.prototype.cmd_handlers = {
 	cmd_help: { name: "help", info: "Shows this help screen" },
 	cmd_status: { name: "status", info: "Shows status information of the server" },
+	cmd_give: { name: 'give', info: "Gives stacks of items" },
+	cmd_items: { name: 'items', info: "List all items" },
 }
 
 // Parses and executes a command
@@ -36,6 +48,16 @@ CommandHandler.prototype.execute = function(user, text) {
 	return null;
 }
 
+CommandHandler.prototype.item_by_name_or_id = function(name) {
+	for (var i = 0; i < this.items.length; i++) {
+		item = this.items[i];
+		if (item.name == name || item.id == name)
+			return item;
+	}
+	
+	return null;
+}
+
 // Commands -----------------------------------------------------------------
 
 CommandHandler.prototype.cmd_help = function(user, args) {
@@ -49,6 +71,23 @@ CommandHandler.prototype.cmd_help = function(user, args) {
 
 CommandHandler.prototype.cmd_status = function(user, args) {
 	return this.mcserver.stats_minejs + this.mcserver.stats_mcserver;
+}
+
+CommandHandler.prototype.cmd_give = function(user, args) {
+	if (args.length != 2)
+		return "invalid params";
+	item = this.item_by_name_or_id(args[0]);
+	if (item == null)
+		return "invalid item";
+	for (var i = 0; i < args[1]; i++)
+		this.mcserver.give(user, item.id, item.stackable ? 64 : 0);
+}
+
+CommandHandler.prototype.cmd_items = function(user, args) {
+	text = "";
+	for (var i = 0; i < this.items.length; i++)
+		text += this.items[i].name + " (" + this.items[i].id + ")\n";
+	return text;
 }
 
 // Creates a command handler
