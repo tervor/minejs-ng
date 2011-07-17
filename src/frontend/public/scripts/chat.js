0ad1e7e4a49903d0a51d4548843a5f24511769b5
Michael Roth <escii@oom.ch>
2011-07-18 00:34:33 +0200

    css

--- src/frontend/public/scripts/chat.js
+++ src/frontend/public/scripts/chat.js
@@ -1,54 +1,59 @@
 function initChat() {
 
-    $(document.body).keypress(function(e) {
-        if (e.which == 13) {
-            var input = $('#chatinput');
-            var text = input.val().toString();
-            if (text.length > 200)
-                text = text.substr(0, 200);
-            if (text.length > 0) {
-                chatWrite(config.user, text);
-                socket.emit('chat', { text: text});
-                input.val('');
-            }
-        }
-		console.log("key hit detected")
+	$(document.body).keypress(function(e) {
+		//force focus on chatinput for any key values
+		$('chatinput').focus();
+
+		//detect ENTER
+		if (e.which == 13) {
+			var input = $('#chatinput');
+			var text = input.val().toString();
+			if (text.length > 200)
+				text = text.substr(0, 200);
+			if (text.length > 0) {
+				chatWrite(config.user, text);
+				socket.emit('chat', { text: text});
+				input.val('');
+			}
+		}
+
+		//show chatbox if hidden on chat events
 		if ($("#chat").is(":hidden")) {
 			$("#chat").show();
 			$('#chatinput').focus();
 		}
 
-    });
+	});
 
-    $('#chattitle').click(function () {
-        $("#chat").hide();
-    });
+	$('#chattitle').click(function () {
+		$("#chat").hide();
+	});
 
-    $('#chattrigger').hover(function() {
+	$('#chattrigger').hover(function() {
 		console.log("chattrigger hover");
-        $("#chat").show();
-    });
+		$("#chat").show();
+	});
 
-    $('#chatinput').blur(function() {
-        $('#chat').hide();
-    });
+	$('#chatinput').blur(function() {
+		$('#chat').hide();
+	});
 
-    $('#chainput').focus();
-    $('#chat')
-        .draggable({ handle: "div.chattitle" })
-        .resizable({ grid: [50, 50] })
-		.hover(
+	$('#chainput').focus();
+	$('#chat')
+			.draggable({ handle: "div.chattitle" })
+			.resizable({ grid: [50, 50] })
+			.hover(
 			function () {
 				$(this).show();
 			},
-  			function () {
+			function () {
 				$(this).hide();
 			}
-		);
+	);
 }
 
 function chatWrite(user, text) {
-    var element = $('#chatoutput');
-    element.html(element.html() + '<b>' + user + '</b> ' + text + "<br/>");
-    element.attr({ scrollTop: element.attr("scrollHeight") });
+	var element = $('#chatoutput');
+	element.html(element.html() + '<b>' + user + '</b> ' + text + "<br/>");
+	element.attr({ scrollTop: element.attr("scrollHeight") });
 }
